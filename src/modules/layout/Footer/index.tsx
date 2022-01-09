91b7c4cbd68d30ab8c7ce61e114cf63c4df2c975
KirillNikoda <nikodakirill386@gmail.com>
2022-01-09 19:53:12 +0300

    feat (modules/layout): add footer

--- src/modules/layout/Footer/index.tsx
+++ src/modules/layout/Footer/index.tsx
@@ -6,7 +6,9 @@ import { HTMLElementProps } from '@modules/core/types/prop.types';
 const Footer = ({ className, ...restProps }: HTMLElementProps<HTMLElement>) => {
   return (
     <footer className={classNames(styles.footer, className)} {...restProps}>
-      footer
+      <p className={styles.text}>OwlTop © 2020 - 2021 Все права защищены</p>
+      <p className={styles.text}>Пользовательское соглашение</p>
+      <p className={styles.text}>Политика конфиденциальности</p>
     </footer>
   );
 };
